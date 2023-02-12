fc1f51be90a90ad3f898a9971b1c851e38fc8a54
Amy Shafer <amy.u.shafer@gmail.com>
2023-02-12 01:10:56 -0500

    changed to postgressql

--- server/connection/config.js
+++ server/connection/config.js
@@ -1,13 +1,27 @@
 const mysql = require("mysql2")
 require('dotenv').config();
 
-const sql = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-)
+// const sql = mysql.createConnection(
+//     {
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME
+//     }
+// )
+
+let sql;  
+  if (process.env.POSTGRESSQL_URL) {
+    sql = process.env.POSTGRESSQL_URL;
+  } else {
+    sql = mysql.createConnection(
+        {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        }
+    )
+  }
 
 module.exports = sql;
